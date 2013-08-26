3f207ba4b5eeb35c38712454933d68523d1ebc40
davidgarvey <david.garvey@gmail.com>
2013-08-26 08:36:16 -0700

    Create check_proc.py

---
+++ check_proc.py
@@ -0,0 +1,13 @@
+import glob
+import os
+
+files = glob.glob("/proc/[0-9]*")
+for file in files:
+    new_path="%s/fd/" %file
+    Target_files = os.listdir(new_path)
+    for fd_file in Target_files:
+        full_path=new_path + fd_file
+        if os.path.islink(full_path):
+            if "/var/log" in os.readlink(full_path):
+                print file
+                print os.readlink(full_path)
