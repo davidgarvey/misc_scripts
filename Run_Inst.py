1e3696726de79dff0fe6ff0638ae73333bfe684c
davidgarvey <david.garvey@gmail.com>
2013-12-26 14:29:54 -0800

    Added the ability to size root partition

--- Run_Inst.py
+++ Run_Inst.py
@@ -8,6 +8,15 @@ User = '<user>' # User with access to aws
 Ami = 'ami for user' # something like 'ami-xxxxxx'
 Instance_Type = 't1.micro' # The size of instance the user can create
 SG = ['<sec group>'] # the security group for user
+dev_sda = boto.ec2.blockdevicemapping.EBSBlockDeviceType()
+dev_sda.size = 100 # size in Gigabytes
+
+
+#Setup block device size object (Note: for linux you need to resize disk once started resize2fs /dev/root-dev)
+bdm = boto.ec2.blockdevicemapping.BlockDeviceMapping()
+bdm['/dev/sda'] = dev_sda
+
+
 
 
 conn = boto.ec2.connect_to_region(Region) 
@@ -23,7 +32,7 @@ name = name.replace('\n','')
 print name
 
 
-reservation = conn.run_instances(Ami, instance_type=Instance_Type, key_name=User, placement=Placement, security_groups=SG)
+reservation = conn.run_instances(Ami, instance_type=Instance_Type, key_name=User, placement=Placement, security_groups=SG, block_device_map = bdm)
 instance = reservation.instances[0]
 status = instance.update()
 while status == 'pending':
