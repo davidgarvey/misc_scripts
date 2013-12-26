6e56adeab2bdf908f584863bc1f1ff9e6807d195
davidgarvey <david.garvey@gmail.com>
2013-12-26 14:40:04 -0800

    Update check_proc.py

--- check_proc.py
+++ check_proc.py
@@ -1,6 +1,8 @@
 import glob
 import os
 
+String = '/var/log/something'
+
 files = glob.glob("/proc/[0-9]*")
 for file in files:
     new_path="%s/fd/" %file
@@ -8,6 +10,6 @@ for file in files:
     for fd_file in Target_files:
         full_path=new_path + fd_file
         if os.path.islink(full_path):
-            if "/var/log" in os.readlink(full_path):
+            if String in os.readlink(full_path):
                 print file
                 print os.readlink(full_path)
