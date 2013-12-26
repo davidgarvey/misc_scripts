0f01e2516a2e34fda7b21dde45452dd75715f781
davidgarvey <david.garvey@gmail.com>
2013-12-26 14:18:47 -0800

    I need to see instances with no name and used SG

--- List_Inst.py
+++ List_Inst.py
@@ -9,3 +9,7 @@ instances = [i for r in reservations for i in r.instances]
 for inst in instances:
     if User in inst.tags:
         print "Name:%s ID:%s  IP:%s State:%s" %(inst.tags['Name'],inst,inst.public_dns_name,inst.state)
+    else:
+        for g in inst.groups: 
+            if g.id == name:
+                print "Name:NoName ID_%s  IP:%s State:%s" %(inst,inst.public_dns_name,inst.state)
